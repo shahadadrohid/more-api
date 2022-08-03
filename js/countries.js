8057c7007c08065f7fc545c9dd9728b2d41abc1e
Mohammad Shahadad Hossain <shahadadrohid@gmail.com>
2022-08-03 22:06:13 +0600

    New commit

--- js/countries.js
+++ js/countries.js
@@ -13,14 +13,34 @@ const displayCountries = data => {
     // console.log(d)
     // }
     data.forEach(d => {
-        console.log(d)
+        // console.log(d)
         const div = document.createElement('div');
+        div.classList.add('country')
         div.innerHTML = `
-        <img src=""></>
         <h2>Name: ${d.name.common}</h2>
         <p>Capital: ${d.capital}</p>
-        <p>Population: ${d.population}</p>
+        <p>Population: ${d.population} </p>
+        <button onclick="loadCountryByName('${d.name.common}')">Show Details</button>
         `
         divContent.appendChild(div)
     })
+}
+
+const loadCountryByName = name => {
+    const url = `https://restcountries.com/v3.1/name/${name}`
+    // console.log(name)
+    // console.log(url)
+    fetch(url)
+        .then(res => res.json())
+        .then(country => displayCountryDetails(country[0]))
+}
+
+const displayCountryDetails = details => {
+    console.log(details)
+    const countryDiv = document.getElementById('country-details')
+    countryDiv.innerHTML = `
+    <img width="200px" src="${details.flags.png}">
+    <h2>Name: ${details.name.common}</h2>
+    <p>Population: ${details.population}</p>
+    `
 }
